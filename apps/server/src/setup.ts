b39eb66937a4d50a2fdf7c40e7a81a81a1b82bc8
Joel Filho <joelfilho02812@gmail.com>
2024-06-30 15:16:37 -0300

    feat[server]: ensure app setup define graphql routes

--- apps/server/src/setup.ts
+++ apps/server/src/setup.ts
@@ -2,11 +2,22 @@ import Koa from "koa";
 import bodyParser from "koa-bodyparser";
 import cors from "kcors";
 import Router from "koa-router";
+import { graphqlHTTP } from "koa-graphql";
 import { contextMiddleware } from "./middlewares";
+import { GQLSchema } from "./schemas";
 
 export const setup = () => {
   const app = new Koa();
   const router = new Router();
+
+  router.all(
+    "/graphql",
+    graphqlHTTP(async (_, __, ctx) => ({
+      schema: GQLSchema,
+      graphiql: true,
+      context: ctx.state.context,
+    }))
+  );
 
   app.use(bodyParser());
   app.use(cors({ origin: "*" }));
