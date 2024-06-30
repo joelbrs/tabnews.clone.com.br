1ea2ac214de3df719f80a9191f30dbf3af1df2ba
Joel Filho <joelfilho02812@gmail.com>
2024-06-30 14:47:22 -0300

    feat[server]: add user mongoose model

---
+++ apps/server/src/modules/user/user-model.ts
@@ -0,0 +1,62 @@
+import { Document, Model, Schema, model } from "mongoose";
+
+export interface IUser extends Document {
+  username: string;
+  email?: string;
+  description?: string;
+  password: string;
+  created_at: Date;
+  updated_at: Date;
+  notify: boolean;
+  tabcoins: number;
+}
+
+export type UserModel = Model<IUser>;
+
+const DEFAULT_TABCOINS = 5;
+
+const UserSchema = new Schema<IUser, UserModel>(
+  {
+    username: {
+      type: String,
+      minlength: 4,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    description: {
+      type: String,
+    },
+    password: {
+      type: String,
+      minlength: 8,
+      required: true,
+    },
+    created_at: {
+      type: Date,
+      default: new Date(),
+    },
+    updated_at: {
+      type: Date,
+      default: new Date(),
+    },
+    notify: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+    tabcoins: {
+      type: Number,
+      default: DEFAULT_TABCOINS,
+    },
+  },
+  {
+    collection: "User",
+    timestamps: true,
+  }
+);
+
+export const User: UserModel = model<IUser, UserModel>("User", UserSchema);
