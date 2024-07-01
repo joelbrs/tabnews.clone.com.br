c5c65cf828fd39238e9f9a2d597e025c4bf8f1ba
Joel Filho <joelfilho02812@gmail.com>
2024-07-01 08:49:56 -0300

    feat[web]: ensure header's link is select based in pathname

--- apps/web/components/unlogged-header.tsx
+++ apps/web/components/unlogged-header.tsx
@@ -1,6 +1,15 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import { InputSearch } from "./input-search";
 
 export function UnloggedHeader(): JSX.Element {
+  const pathname = usePathname();
+
+  const pathnameIsSelected = (path: string) => {
+    return (pathname === path && "underline underline-offset-4") || "";
+  };
+
   return (
     <header className="text-white font-medium text-sm bg-[#161b22] p-3.5">
       <div className="flex justify-between items-center">
@@ -8,27 +17,35 @@ export function UnloggedHeader(): JSX.Element {
           <div className="flex items-center gap-2 hover:cursor-pointer hover:text-zinc-400">
             <div className="flex items-center gap-5">
               <h3 className="hidden md:flex">TabNews</h3>
-              <a className="underline underline-offset-4" href="/">
+              <a className={pathnameIsSelected("/")} href="/">
                 Relevantes
               </a>
             </div>
           </div>
 
-          <a href="/">Recentes</a>
+          <a className={pathnameIsSelected("/recentes")} href="/recentes">
+            Recentes
+          </a>
         </nav>
 
         <div className="flex items-center gap-5">
           <InputSearch />
 
           <div className="md:hidden">
-            <a href="/">Entrar</a>
+            <a className={pathnameIsSelected("/login")} href="/login">
+              Entrar
+            </a>
           </div>
 
           <div className="hidden md:flex">
-            <a href="/">Login</a>
+            <a className={pathnameIsSelected("/login")} href="/login">
+              Login
+            </a>
           </div>
           <div className="hidden md:flex">
-            <a href="/">Cadastrar</a>
+            <a className={pathnameIsSelected("/cadastro")} href="/cadastro">
+              Cadastrar
+            </a>
           </div>
         </div>
       </div>
