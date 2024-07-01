e21f9313030aa6e379231ac71d76f1d9acfcbb91
Joel Filho <joelfilho02812@gmail.com>
2024-07-01 16:05:32 -0300

    feat[web]: ensure headers links activate when url is accessed

--- apps/web/components/unlogged-header.tsx
+++ apps/web/components/unlogged-header.tsx
@@ -1,6 +1,15 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import { InputSearch } from "./input-search";
 
 export function UnloggedHeader(): JSX.Element {
+  const pathname = usePathname();
+
+  const isActive = (path: string) => {
+    return (pathname === path && "underline underline-offset-4") || "";
+  };
+
   return (
     <header className="text-white font-medium text-sm bg-[#161b22] p-3.5">
       <div className="flex justify-between items-center">
@@ -8,28 +17,31 @@ export function UnloggedHeader(): JSX.Element {
           <div className="flex items-center gap-2 hover:cursor-pointer hover:text-zinc-400">
             <div className="flex items-center gap-5">
               <h3 className="hidden md:flex">TabNews</h3>
-              <a className="underline underline-offset-4" href="/">
+              <a className={isActive("/")} href="/">
                 Relevantes
               </a>
             </div>
           </div>
 
-          <a href="/">Recentes</a>
+          <a className={isActive("/recentes")} href="/recentes">
+            Recentes
+          </a>
         </nav>
 
         <div className="flex items-center gap-5">
           <InputSearch />
 
-          <div className="md:hidden">
-            <a href="/">Entrar</a>
-          </div>
+          <a className={`${isActive("/login")} md:hidden`} href="/login">
+            Entrar
+          </a>
 
-          <div className="hidden md:flex">
-            <a href="/">Login</a>
-          </div>
-          <div className="hidden md:flex">
-            <a href="/">Cadastrar</a>
-          </div>
+          <a className={isActive("/login")} href="/login">
+            Login
+          </a>
+
+          <a className={isActive("/cadastro")} href="/cadastro">
+            Cadastrar
+          </a>
         </div>
       </div>
     </header>
