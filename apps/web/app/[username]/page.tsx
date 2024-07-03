7852102b222c2a3f1ce0a7fab6fc634073bc8760
Joel Filho <joelfilho02812@gmail.com>
2024-07-02 21:32:21 -0300

    feat[web]: ensure render publishes count when tab is selected

--- apps/web/app/[username]/page.tsx
+++ apps/web/app/[username]/page.tsx
@@ -14,6 +14,7 @@ import { PublishesTab } from "./_publishes-tab";
 
 export default function PerfilPage(): JSX.Element {
   const [user, setUser] = useState<User>();
+  const [tab, setTab] = useState(0);
 
   const params = useSearchParams();
 
@@ -39,14 +40,16 @@ export default function PerfilPage(): JSX.Element {
           {/** Settings Component */}
         </div>
 
-        <Tabs className="w-full">
+        <Tabs className="w-full" onSelect={(tab) => setTab(tab)}>
           <TabList>
             <Tab>Perfil</Tab>
             <Tab>
               Publicações{" "}
-              <span className="text-xs px-2 bg-slate-200 rounded-full font-bold">
-                {user?.posts?.length}
-              </span>
+              {tab === 1 && (
+                <span className="text-xs px-2 bg-slate-200 rounded-full font-bold">
+                  {user?.posts?.length}
+                </span>
+              )}
             </Tab>
           </TabList>
 
