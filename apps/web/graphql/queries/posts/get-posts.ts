418834692526561dc214f9e503f0db07574dc5b9
Joel Filho <joelfilho02812@gmail.com>
2024-07-03 10:26:23 -0300

    feat[web]: add slug variable in posts query

--- apps/web/graphql/queries/posts/get-posts.ts
+++ apps/web/graphql/queries/posts/get-posts.ts
@@ -1,8 +1,8 @@
 import { graphql } from "relay-runtime";
 
 export const GetPostsQuery = graphql`
-  query getPostsQuery($page: Int, $limit: Int) {
-    GetPosts(page: $page, limit: $limit) {
+  query getPostsQuery($page: Int, $limit: Int, $slug: String) {
+    GetPosts(page: $page, limit: $limit, slug: $slug) {
       edges {
         node {
           title
