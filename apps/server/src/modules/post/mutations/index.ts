e6597a12722eb9a4290354bcae0449a326e532a7
Joel Filho <joelfilho02812@gmail.com>
2024-07-05 11:10:27 -0300

    feat[server]: add update post mutation

--- apps/server/src/modules/post/mutations/index.ts
+++ apps/server/src/modules/post/mutations/index.ts
@@ -1,7 +1,9 @@
 import { CreatePostMutation as CreatePost } from "./create-post";
+import { UpdatePostMutation as UpdatePost } from "./update-post";
 import { VotePostMutation as VotePost } from "./vote-post";
 
 export const PostMutations = {
   CreatePost,
   VotePost,
+  UpdatePost,
 };
